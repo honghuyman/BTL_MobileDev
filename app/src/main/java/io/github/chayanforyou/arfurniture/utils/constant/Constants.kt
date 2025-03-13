e5d9cddc186e493117a08d58ad87d68c9ce5d042
Chayan Mistry <chayanmistrry@gmail.com>
2025-03-13 11:34:16 +0600

    commit changes

--- app/src/main/java/io/github/chayanforyou/arfurniture/utils/constant/Constants.kt
+++ app/src/main/java/io/github/chayanforyou/arfurniture/utils/constant/Constants.kt
@@ -3,27 +3,6 @@ package io.github.chayanforyou.arfurniture.utils.constant
 object Constants {
 
     /**
-     * Database
-     */
-    const val DATABASE = "ssfurnicraftar-database"
-    const val TABLE_PRODUCT = "product"
-    const val TABLE_CATEGORY = "category"
-    const val TABLE_CATEGORY_PRODUCT = "category_product"
-    const val TABLE_REMOTE_KEY = "remote_key"
-
-    /**
-     * Authenticator
-     */
-    const val HEADER_AUTHORIZATION = "Authorization"
-    const val TOKEN_KEY = "Token"
-
-    /**
-     * Download
-     */
-    const val DOWNLOAD_PROGRESS_INTERVAL = 500 // MS
-    const val LABEL_DOWNLOAD_PROGRESS = "DownloadProgress"
-
-    /**
      * Model
      */
     const val MODEL_EXTENSION = "glb"
